91f9285410a5a44fec8dadcd2c8a1982a134c2fd
Sabrina C. Sales <sacsales@icloud.com>
2023-08-21 19:08:06 +0400

    Initial Commit

---
+++ TabbyCat/TabbyCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbyCatApp.swift
+//  TabbyCat
+//
+//  Created by Sabrina C. Sales on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TabbyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
